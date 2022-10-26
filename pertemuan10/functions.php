5e43bd887a25090f5ea201f86358ec7dabfc667d
Fadlanprabaswara <prabaswarafadlan@gmail.com>
2022-10-26 15:27:06 +0700

    menambahkan fitur tambah data mahasiswa

--- pertemuan10/functions.php
+++ pertemuan10/functions.php
@@ -23,3 +23,25 @@ function query($query)
 
   return $rows;
 }
+
+function tambah($data)
+{
+  $conn = koneksi();
+
+  $nama = htmlspecialchars($data['name']);
+  $npm = htmlspecialchars($data['npm']);
+  $email = htmlspecialchars($data['email']);
+  $jurusan = htmlspecialchars($data['jurusan']);
+  $gambar = htmlspecialchars($data['gambar']);
+
+  $query = "INSERT INTO
+              mahasiswa
+            VALUES
+            (null, '$nama', '$npm', '$email', '$jurusan', '$gambar');
+          ";
+  mysqli_query($conn, $query);
+
+  //mengembalikan nilai
+  echo mysqli_error($conn);
+  return mysqli_affected_rows($conn);
+}
